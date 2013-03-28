9581b381328be7b2453f76e6532b29df4b6ebf07
jaric <mm1000@list.ru>
2013-03-28 17:24:28 +0400

    grammatical error has been fixed

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ exports.fight = function(req, res){
     {
         var db = mongoose.connection;
 
-        db.on('error', console.error.bind(console, 'ZZZconnection error:'));
+        db.on('error', console.error.bind(console, 'connection error:'));
         db.once('open', function(err)
         {
             if (!err)
